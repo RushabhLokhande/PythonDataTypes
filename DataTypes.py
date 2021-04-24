f2231d001ab6bc606db281d4f13bc0feebb7acef
RushabhLokhande <rushabhlokhande7171@gmail.com>
2021-04-24 01:45:53 -0700

    first commit

---
+++ DataTypes.py
@@ -0,0 +1,9 @@
+i , j, k = 10,20.2,'Hello'
+
+print(i)
+print(j)
+print(k)
+
+print(type(i))
+print(type(j))
+print(type(k))
---
+++ SpecifyType.py
@@ -0,0 +1,7 @@
+i = int(30)
+print(i)
+print(type(i))
+
+j = float(20.5)
+print(j)
+print(type(j))
